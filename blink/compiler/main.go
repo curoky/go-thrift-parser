8fc0fbd6fa33b023e4eecb0147c8f5f872499601
curoky <cccuroky@gmail.com>
2021-07-17 19:17:53 +0800

    feat(compiler): implement command line parameters

--- blink/compiler/main.go
+++ blink/compiler/main.go
@@ -16,6 +16,69 @@
 
 //go:generate pigeon -o parser/thrift.peg.go parser/thrift.peg
 //go:generate thrift --gen go --out . ast/ast.thrift
+
 package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/curoky/blink/blink"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+)
+
+func app() *cli.App {
+	app := &cli.App{
+		Name:    "blink compiler",
+		Usage:   "compiler thrift IDL",
+		Version: fmt.Sprintf("%s (%s)", blink.Version, runtime.Version()),
+	}
+	app.Flags = []cli.Flag{
+		&cli.PathFlag{
+			Name:      "file",
+			Usage:     "input file path",
+			TakesFile: true,
+			Required:  true,
+		},
+		&cli.PathFlag{
+			Name:     "out",
+			Usage:    "output directory",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "lang",
+			Usage:    "language need to generate",
+			Required: true,
+		},
+		&cli.BoolFlag{
+			Name:  "dump",
+			Value: false,
+			Usage: "dump ast",
+		},
+		&cli.BoolFlag{
+			Name:  "readonly",
+			Value: true,
+			Usage: "make output file readony",
+		},
+		&cli.BoolFlag{
+			Name:   "fmtcode",
+			Value:  true,
+			Usage:  "format code",
+			Hidden: true,
+		},
+		&cli.BoolFlag{
+			Name:  "verbose",
+			Value: false,
+		},
+	}
+	return app
+}
+
 func main() {
+	err := app().Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
