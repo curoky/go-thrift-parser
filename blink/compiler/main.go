d5858ac982cd77573eb922687a91a54699e2ef12
curoky <cccuroky@gmail.com>
2022-04-09 16:14:32 +0800

    feat(compiler): remove tricks for packr

--- blink/compiler/main.go
+++ blink/compiler/main.go
@@ -17,7 +17,6 @@
 //go:generate pigeon -o parser/thrift.peg.go parser/thrift.peg
 //go:generate thrift --gen go --out . ast/ast.thrift
 //go:generate packr2
-//go:generate ./fix-packr.sh
 
 package main
 
